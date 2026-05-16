34f55685222210338f149565fda1055a9bff0da2
kostromin-s <nam.lh220118@sis.hust.edu.vn>
2026-05-16 12:02:46 +0700

    dfsd

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,10 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://stock-chatbot-fpt.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
